550096b643e575ffbdf7790de33d374b5badae25
ogudino <osvaldo.gudino@icloud.com>
2021-10-12 19:11:16 -0500

    BST min/max/level order transversal

--- java/src/main/java/xyz/ogudino/algorithms/bst/BinarySearchTree.java
+++ java/src/main/java/xyz/ogudino/algorithms/bst/BinarySearchTree.java
@@ -1,5 +1,10 @@
 package xyz.ogudino.algorithms.bst;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class BinarySearchTree<T extends Comparable<T>> {
 
     public static class Node<T> {
@@ -26,11 +31,67 @@ public class BinarySearchTree<T extends Comparable<T>> {
         }
 
         if (data.compareTo(node.data) < 0) {
-            insert(node.left, data, node);
-        } else {
-            insert(node.right, data, node);
+            node.left = insert(node.left, data, node);
+        } else if (data.compareTo(node.data) > 0) {
+            node.right = insert(node.right, data, node);
         }
         return node;
+    }
+
+    public Node<T> search(T data) {
+        return search(root, data);
+    }
+
+    private Node<T> search(Node<T> node, T data) {
+        if (node == null) {
+            return null;
+        } else if (node.data.equals(data)) {
+            return node;
+        } else if (data.compareTo(node.data) < 0) {
+            return search(node.left, data);
+        } else {
+            return search(node.right, data);
+        }
+    }
+
+    public Node<T> min() {
+        Node<T> min = root;
+
+        while (min != null && min.left != null) {
+            min = min.left;
+        }
+
+        return min;
+    }
+
+    public Node<T> max() {
+        Node<T> max = root;
+
+        while (max != null && max.right != null) {
+            max = max.right;
+        }
+
+        return max;
+    }
+
+    public List<T> levelOrderTransversal() {
+        List<T> list = new ArrayList<>();
+
+        Queue<Node<T>> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            Node<T> node = queue.poll();
+            list.add(node.data);
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+
+        return list;
     }
 
 }
--- java/src/test/java/xyz/ogudino/algorithms/bst/BinarySearchTreeTest.java
+++ java/src/test/java/xyz/ogudino/algorithms/bst/BinarySearchTreeTest.java
@@ -1,13 +1,18 @@
 package xyz.ogudino.algorithms.bst;
 
 import org.junit.jupiter.api.Test;
+import xyz.ogudino.algorithms.bst.BinarySearchTree.Node;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class BinarySearchTreeTest {
 
     @Test
-    void insert() {
+    public void insert() {
         BinarySearchTree<Integer> tree = new BinarySearchTree<>();
         tree.insert(0);
         assertEquals(0, tree.root.data);
@@ -15,5 +20,59 @@ class BinarySearchTreeTest {
         assertEquals(-1, tree.root.left.data);
         tree.insert(1);
         assertEquals(1, tree.root.right.data);
+
+        List<Integer> levels = tree.levelOrderTransversal();
+        List<Integer> expected = new ArrayList<>();
+        expected.add(0);
+        expected.add(-1);
+        expected.add(1);
+        assertEquals(expected, levels);
+    }
+
+    @Test
+    public void search() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        tree.insert(0);
+        assertEquals(0, tree.root.data);
+        tree.insert(-1);
+        assertEquals(-1, tree.root.left.data);
+        tree.insert(1);
+        assertEquals(1, tree.root.right.data);
+
+        Node<Integer> node = tree.search(-1);
+        assertEquals(-1, node.data);
+
+        node = tree.search(Integer.MAX_VALUE);
+        assertNull(node);
+    }
+
+    @Test
+    public void min() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        assertNull(tree.min());
+        tree.insert(-1);
+        assertEquals(-1, tree.min().data);
+        tree.insert(1);
+        assertEquals(-1, tree.min().data);
+        tree.insert(-1000);
+        tree.insert(-100);
+        tree.insert(-100);
+        tree.insert(-10);
+        assertEquals(-1000, tree.min().data);
+    }
+
+    @Test
+    public void max() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        assertNull(tree.max());
+        tree.insert(-1);
+        assertEquals(-1, tree.max().data);
+        tree.insert(1);
+        assertEquals(1, tree.max().data);
+        tree.insert(-1000);
+        tree.insert(-100);
+        tree.insert(-100);
+        tree.insert(-10);
+        assertEquals(1, tree.max().data);
     }
 }
